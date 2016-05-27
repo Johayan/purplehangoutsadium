f7baf1c5735908c77daef1716983f5175681e7da
John Lamb <john@johns-imac.local>
2016-05-27 03:53:00 -0500

    Initial Commit

---
+++ purple-hangouts/purple-hangouts.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <Adium/AIPlugin.h>
+#import <Adium/AISharedAdium.h>
+
+@interface purple-hangouts : AIPlugin {
+
+}
+
+@end
+	
---
+++ purple-hangouts/purple-hangouts.m
@@ -0,0 +1,18 @@
+#import "purple-hangouts.h"
+
+@implementation purple-hangouts
+
+- (void)installPlugin
+{
+  // Do something for when plugin is installed
+  NSLog(@"** Installed purple-hangouts Plugin");
+}
+
+- (void)uninstallPlugin
+{
+  // Do something for when plugin is uninstalled
+  NSLog(@"** Uninstalled purple-hangouts Plugin");
+}
+
+@end
+	
